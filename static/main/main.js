83696e1449c84ff023f60c64fad66b55ead3ccd2
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-07-31 20:54:06 +0200

    add expanded map front-end

--- static/main/main.js
+++ static/main/main.js
@@ -1,8 +1,12 @@
 import * as _ from "../lib/componentLoader.js";
 
 const miniMap = document.getElementById("miniMap");
+const bigMap = document.getElementById("bigMap");
 
 await uiBuilder.ready(miniMap);
+await uiBuilder.ready(bigMap);
 
-//miniMap.component.map.style.height = "100px";
 await miniMap.component.init();
+
+bigMap.component.mapElem.style.height = "400px";
+await bigMap.component.init();
